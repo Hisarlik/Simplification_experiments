183d0208cb6160c589d298965d3af5c396e6b7c9
Antonio Menta <mentared@gmail.com>
2022-01-06 22:56:31 +0100

    Starting data functions. load_dataset_into_pandas

--- main_edit_nts.py
+++ main_edit_nts.py
@@ -1,5 +1,22 @@
+import sys
+
+sys.dont_write_bytecode = True
+from pathlib import Path
+
+from src.data import storage
+from src.utils import logging_module
+
+logger = logging_module.get_logger(__name__)
+
+
+path = "data/wikismall/PWKP_108016.tag.80.aner.ori.train.src"
+
+
 def main():
-    print("Working main editNTS")
+    logger.info("Working main editNTS")
+    pathfile = Path(path)
+    df = storage.load_dataset_into_pandas(pathfile, pathfile, "\t")
+    print(df.head())
 
 
 if __name__ == "__main__":
