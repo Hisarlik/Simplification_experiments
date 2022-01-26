5faf4f92167a6e50d7476f9e4389f837b7943f95
Antonio Menta <mentared@gmail.com>
2022-01-26 10:13:48 +0100

    Fix Lexical Simplification notebook

--- main_edit_nts.py
+++ main_edit_nts.py
@@ -3,20 +3,19 @@ import sys
 sys.dont_write_bytecode = True
 from pathlib import Path
 
-from src.data import storage
+from src.data import preprocess
 from src.utils import logging_module
+from src.constants import TRAIN_ORIGINAL_DATA_PATH,TRAIN_SIMPLE_DATA_PATH
 
 logger = logging_module.get_logger(__name__)
 
-
-path = "data/wikismall/PWKP_108016.tag.80.aner.ori.train.src"
-
-
 def main():
-    logger.info("Working main editNTS")
-    pathfile = Path(path)
-    df = storage.load_dataset_into_pandas(pathfile, pathfile, "\t")
-    print(df.head())
+    logger.info("Init editNTS")
+
+    df = preprocess.preprocess_raw_data(
+        Path(TRAIN_ORIGINAL_DATA_PATH), Path(TRAIN_SIMPLE_DATA_PATH)
+    )
+    print(df)
 
 
 if __name__ == "__main__":
---
+++ src/constants.py
@@ -0,0 +1,23 @@
+import torch
+
+TRAIN_ORIGINAL_DATA_PATH = "data/wikismall/PWKP_108016.tag.80.aner.ori.train.src"
+TRAIN_SIMPLE_DATA_PATH = "data/wikismall/PWKP_108016.tag.80.aner.ori.train.dst"
+VOCAB_POS_TAGGING_PATH = "data/pos_tagging/postag_set.p"
+
+
+
+PAD = 'PAD' #  This has a vocab id, which is used to represent out-of-vocabulary words [0]
+UNK = 'UNK' #  This has a vocab id, which is used to represent out-of-vocabulary words [1]
+KEEP = 'KEEP' # This has a vocab id, which is used for copying from the source [2]
+DEL = 'DEL' # This has a vocab id, which is used for deleting the corresponding word [3]
+START = 'START' # this has a vocab id, which is uded for indicating start of the sentence for decoding [4]
+STOP = 'STOP' # This has a vocab id, which is used to stop decoding [5]
+
+PAD_ID = 0 #  This has a vocab id, which is used to represent out-of-vocabulary words [0]
+UNK_ID = 1 #  This has a vocab id, which is used to represent out-of-vocabulary words [1]
+KEEP_ID = 2 # This has a vocab id, which is used for copying from the source [2]
+DEL_ID = 3 # This has a vocab id, which is used for deleting the corresponding word [3]
+START_ID = 4 # this has a vocab id, which is uded for indicating start of the sentence for decoding [4]
+STOP_ID = 5 # This has a vocab id, which is used to stop decoding [5]
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
